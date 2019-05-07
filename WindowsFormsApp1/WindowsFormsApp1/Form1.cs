f4ffea86d37c3117b4360a2eff496c5471d4af16
Hideo Kojima <kostyaelizarov@mail.ru>
2019-05-07 23:22:03 +0300

    trisemus

--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -15,10 +15,10 @@ namespace WindowsFormsApp1
         private string alphabet = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"; //33 - 36   КЛАССИЧЕСКИЙ АЛФАВИТ
         private string newalphabet; // ДЛЯ ОПЕРАЦИЙ С АЛФАВИТОМ
         private char[,] polimass = new char[6,6];
-        private string key; //КЛЮЧ ШИФРА
         private string firstWord; //ВХОДНОЕ СЛОВО
         private string secondWord; //ВЫХОДНОЕ СЛОВО
         private char CH; //Символ Алфавита
+        private string key;
         private int scifernum;//НОМЕР ОПЕРАЦИИ
         public Form1()
         {
@@ -35,6 +35,8 @@ namespace WindowsFormsApp1
         {
             Chifer(false);
         }
+
+
         private void check(object sender, EventArgs e)
         {
             if (radioButton1.Checked) { textBox1.Visible = false; scifernum = 1; } 
@@ -46,6 +48,8 @@ namespace WindowsFormsApp1
             if (radioButton7.Checked) { textBox1.Visible = true; scifernum = 7; }
             if (radioButton8.Checked) { textBox1.Visible = true; scifernum = 8; }
         }
+
+
         private void Chifer(bool flagShif)
         {
             key = textBox1.Text;
@@ -61,6 +65,7 @@ namespace WindowsFormsApp1
                     polibian(flagShif);
                         break;
                     case 4:
+                    trisemus(flagShif);
                         break;
                     case 5:
                         break;
@@ -73,7 +78,10 @@ namespace WindowsFormsApp1
             }
       
         }
-        private void czeesar(bool flag) {
+
+
+        private void czeesar(bool flag)//АТБАШ
+        {
             for (int i = 3; i < alphabet.Length; i++) //присваиваем символы кроме первых трёх в начало строки
             {
                 newalphabet += alphabet[i];
@@ -105,7 +113,8 @@ namespace WindowsFormsApp1
             newalphabet = ""; //обнуляем
         }
 
-        private void adbash(bool flag)//ПОЛИБЕАНСКИЙ КВАДРАТ
+
+        private void adbash(bool flag)//АТБАШ
         {
             for (int i = alphabet.Length - 1; i > -1; i--) { //Тупо переворачиваем строку можно реверсом, но я решил так
                 newalphabet += alphabet[i];             
@@ -129,15 +138,17 @@ namespace WindowsFormsApp1
             secondWord = "";
             newalphabet = ""; //обнуляем
         }
+
+
         private void polibian(bool flag)//ПОЛИБЕАНСКИЙ КВАДРАТ
         {
             int k=0;
             newalphabet += alphabet + "./_"; // дополняем до 36 символов
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < 6; i++)//двумерному массиву присваиваем алфавит
             {
                 for (int j = 0; j < 6; j++)
                 {
-                    polimass[i, j] = newalphabet[k++];  //двумерному массиву присваиваем алфавит
+                    polimass[i, j] = newalphabet[k++];  
                 }
             }
             k = 0;
@@ -157,9 +168,74 @@ namespace WindowsFormsApp1
             }
             else
             {
-                
+                for (int i = 0; i < firstWord.Length - 1; i = i + 2) {
+                    secondWord += polimass[Convert.ToInt32(Convert.ToString(firstWord[i]))-1, Convert.ToInt32(Convert.ToString(firstWord[i + 1]))-1];                   
+                }
                 textBox3.Text = secondWord;
             }
+            secondWord = "";
+            newalphabet = ""; //обнуляем
+        }
+
+
+        private void trisemus(bool flag)//ТАБЛИЦА ТРИСЕМУСА
+        {
+            int k = 0;
+            string newkey = Convert.ToString(key[0]);
+            for (int i=0; i < key.Length; i++) // Убираем лишние символы
+            {
+                if (newkey.Contains(key[i])) continue;
+                newkey += key[i];
+            }
+            newalphabet = newkey;
+            for (int i = 0; i < alphabet.Length; i++) // генерируем новый алфавит(чтобы вставить его в матрицу)
+            {
+                if (newalphabet.Contains(alphabet[i])) continue;
+                newalphabet += alphabet[i];
+            }
+            newalphabet += "./!";
+            for (int i = 0; i < 6; i++)//двумерному массиву присваиваем новый алфавит
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    polimass[i, j] = newalphabet[k++];  
+                }
+            }
+            k = 0;
+
+            if (flag)
+            {             
+                for (int i = 0; i < firstWord.Length; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        for (int n = 0; n < 6; n++)
+                        {
+                            if (polimass[j, n] == firstWord[i])
+                            {
+                                secondWord += (j != 5) ? polimass[j + 1, n] : polimass[0, n];
+                            }
+                        }
+                    }
+                }
+            }
+            else
+            {
+                for (int i = 0; i < firstWord.Length; i++)
+                {
+                    for (int j = 0; j < 6; j++)
+                    {
+                        for (int n = 0; n < 6; n++)
+                        {
+                            if (polimass[j, n] == firstWord[i])
+                            {
+                                secondWord += (j != 0) ? polimass[j - 1, n] : polimass[5, n];
+                            }
+                        }
+                    }
+                }
+            }
+            textBox3.Text = secondWord;
             secondWord = "";
             newalphabet = ""; //обнуляем
         }
